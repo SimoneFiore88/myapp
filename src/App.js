047ffc5ef234d6bbb729d465f6feec87cdf86770
Simone Fiore <simone.fiore@aulab.it>
2021-07-01 12:14:07 +0200

    basename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
       <Navbar />           
 
 
